363204db2ac019d95b88fd39623ae3bd162c122d
JUJU-desu <JUJU-desu@users.noreply.github.com>
2026-09-20 13:06:54 +0800

    Extract Argon theme packages from monorepo

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -47,8 +47,7 @@ UPDATE_PACKAGE() {
 
 # UPDATE_PACKAGE "包名" "项目地址" "项目分支" "pkg，可选，从大杂烩中单独提取包名插件"
 # 主题
-UPDATE_PACKAGE "argon" "sbwml/luci-theme-argon" "openwrt-25.12"
-UPDATE_PACKAGE "argon-config" "sbwml/luci-app-argon-config" "master"
+UPDATE_PACKAGE "argon" "sbwml/luci-theme-argon" "openwrt-25.12" "pkg"
 
 # 只准备手动安装的插件源码；它们由配置设为 m，编译成 apk 但不进入固件
 UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
